bb83997df7fe5f6020a93aa99fd63d5c875555e2
Marco De Canal <marco.decanal@gmail.com>
2022-01-02 12:27:58 +0100

    mongoose connection

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 const express = require("express");
 const morgan = require("morgan");
-const userRouter = require("./routes/userRoutes");
 const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
+const userRouter = require("./routes/userRoutes");
 
 dotenv.config({ path: "./config.env" });
+const env = process.env.NODE_ENV;
+
+const MongoDB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(MongoDB, { useNewUrlParser: true })
+    .then(
+      (con) => {
+        env === "development" && console.log('Connected to MongoDB Atlas - ilsalice DB');
+        return con;
+      },
+      (err) => {
+        env === "development" && console.error(`Error connecting to MongoDB Atlas:
+    ${err}`);
+        return err;
+      }
+    )
+    .catch((err) =>
+    env === "development" && console.error(`Error connecting to MongoDB Atlas:
+  ${err}`)
+    );
+
+    if (env === "development") {
+      mongoose.connection.on('error', (err) =>
+      console.error(`There was an error in the connection with MongoDB Atlas:
+    ${err}`)
+    );
+    mongoose.connection.on('disconnected', () =>
+      console.log('Disconnected from MongoDB Atlas')
+    );
+  }
+
 const app = express();
+
 
 // MIDDLEWARES
 app.use(express.json());
