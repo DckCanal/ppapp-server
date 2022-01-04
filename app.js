33d2f2d33be01a90b0c74dfb35bfa1fd92c6d08b
Marco De Canal <marco.decanal@gmail.com>
2022-01-04 09:41:23 +0100

    double quotes...

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userRouter = require("./routes/userRoutes");
 
@@ -9,40 +9,43 @@ dotenv.config({ path: "./config.env" });
 const env = process.env.NODE_ENV;
 
 const MongoDB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
 
 mongoose
-    .connect(MongoDB, { useNewUrlParser: true })
-    .then(
-      (con) => {
-        env === "development" && console.log('Connected to MongoDB Atlas - ilsalice DB');
-        return con;
-      },
-      (err) => {
-        env === "development" && console.error(`Error connecting to MongoDB Atlas:
+  .connect(MongoDB, { useNewUrlParser: true })
+  .then(
+    (con) => {
+      env === "development" &&
+        console.log("Connected to MongoDB Atlas - ilsalice DB");
+      return con;
+    },
+    (err) => {
+      env === "development" &&
+        console.error(`Error connecting to MongoDB Atlas:
     ${err}`);
-        return err;
-      }
-    )
-    .catch((err) =>
-    env === "development" && console.error(`Error connecting to MongoDB Atlas:
+      return err;
+    }
+  )
+  .catch(
+    (err) =>
+      env === "development" &&
+      console.error(`Error connecting to MongoDB Atlas:
   ${err}`)
-    );
+  );
 
-    if (env === "development") {
-      mongoose.connection.on('error', (err) =>
-      console.error(`There was an error in the connection with MongoDB Atlas:
+if (env === "development") {
+  mongoose.connection.on("error", (err) =>
+    console.error(`There was an error in the connection with MongoDB Atlas:
     ${err}`)
-    );
-    mongoose.connection.on('disconnected', () =>
-      console.log('Disconnected from MongoDB Atlas')
-    );
-  }
+  );
+  mongoose.connection.on("disconnected", () =>
+    console.log("Disconnected from MongoDB Atlas")
+  );
+}
 
 const app = express();
-
 
 // MIDDLEWARES
 app.use(express.json());
